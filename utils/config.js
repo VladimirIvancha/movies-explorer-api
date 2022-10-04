e406ae6f0828f5a4c3ec136dcf923607417ae7f5
Vladimir Ivancha <Vladimir.Ivancha@mail.ru>
2022-10-04 11:47:06 +0300

    refactor: port changed

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 const mongodbServer = 'mongodb://localhost:27017/moviesdb';
 const secretTokenKey = 'secret-key';
-const port = 3001;
+const port = 3000;
 
 const jwtSettings = {
   expiresIn: '7d',
